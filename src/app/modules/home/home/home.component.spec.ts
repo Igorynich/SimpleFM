e1b6d70b192ccc49effd7cf36183f4a2188c6831
Igorynich <igorsp@mail.ru>
2020-05-21 15:54:29 +0300

    default tests fixed

--- src/app/modules/home/home/home.component.spec.ts
+++ src/app/modules/home/home/home.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {Router} from '@angular/router';
+import {UserService} from '../../../services/user.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        {provide: Router, useValue: routerSpy},
+        UserService
+      ]
     })
     .compileComponents();
   }));
--- src/app/modules/office/main-page/main-page.component.spec.ts
+++ src/app/modules/office/main-page/main-page.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainPageComponent } from './main-page.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../../../services/user.service';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
 
   beforeEach(async(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const activatedRouteStub = {};
     TestBed.configureTestingModule({
-      declarations: [ MainPageComponent ]
+      declarations: [ MainPageComponent ],
+      providers: [
+        {provide: Router, useValue: routerSpy},
+        UserService,
+        {provide: ActivatedRoute, useValue: activatedRouteStub}
+      ]
     })
     .compileComponents();
   }));
